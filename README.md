3f7e738ca3f57a94edacfca1354c7bf1f0c4b810
tsvetkovpro <285019@gmail.com>
2016-02-16 10:48:51 +0300

    установка и запуск задач
    задача может сигнализировать о завершении одним из способов:
    
    1. вызвать callback
    
    gulp.task("hello", function (callback) {
    	console.log("Hello!");
    	callback();
    });
    
    задача использует callback чтобы сигнализировать о завершении,
    но таже она может возвращать promise/ gulp будет следить за ним)
    
     gulp.task('example:promise', function() {
    	return new Promise((resolve, reject) => {
    		// ....
    		resolve('result');
    		});
    	});
    
    таже задача может возвращать поток?\:
    
    gulp.task('example:stream', function() {
    	//прочитать всё из потока и сзатем сделать
    	return require('fs').createReadStream(__filename);
    	});
    
    	или же задача может порождать новый дочерний процесс
    
    gulp.task('example:process', function() {
    	//return child process
    	return require('child_process').spawn('ls', ['.'], {stdio: 'inherit'});
    	});
    и возвращать его.
    
    gulp также позволяет создавать новые задачи  используя уже
    объявленные:
    
    gulp.task('example', gulp.series('hello', 'example:promise', 'example:stream', 'example:process'));
    
    gulp не позволяет передавать данные между задачами.
    
    например gulp читает поток, но ничего не делает с ним, он прочитанное не использует ждёт его завершения

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 create gulpfile for building system
+
+
+
+cmd windows path for using gulp localy (and git shell of cource based localy in smartgit software directory):
+"c:\Program Files (x86)\SmartGit\git\bin\";.\node_modules\.bin;..\node_modules\.bin;..\..\node_modules\.bin
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,29 @@
 
 const gulp = require('gulp');
 
-gulp.task("d:d", function (callback) {
+gulp.task("hello", function (callback) {
 	console.log("Hello!");
 	callback();
 });
+
+gulp.task('example:promise', function() {
+	return new Promise((resolve, reject) => {
+		// ....
+		resolve('result');
+		});
+	});
+
+gulp.task('example:stream', function() {
+	//прочитать всё из потока и сзатем сделать
+	return require('fs').createReadStream(__filename);
+	});
+
+gulp.task('example:process', function() {
+	//return child process
+	return require('child_process').spawn('ls', ['.'], {stdio: 'inherit'});
+	});
+
+gulp.task('example', gulp.series('hello', 'example:promise', 'example:stream', 'example:process'));
+
+//параллельные задачи
+gulp.task('example-pll', gulp.parallel('hello', 'example:promise', 'example:stream', 'example:process'));
