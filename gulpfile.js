338221f23f47b4f261e3c6ae0e0e0d0b41977988
tsvetkovpro <285019@gmail.com>
2016-02-17 17:44:36 +0300

    debug() - пропускает всё через себя и выводит что чере него прошло
    у debug()  есть заголовок  {title:
    
    Начальная сборка стилей:
    
    src: frontend\styles\header.styl - оно получило src, передался дальше
    stylus: frontend\styles\header.css - результат stylus
    src: frontend\styles\main.styl
    stylus: frontend\styles\main.css
    src: 2 items - завершился поток src всего два элемента/ мы помним что плагин concat ждёт пока ему будет всё передано. т.е. он ждёт завершения передачи ему файлов, завершении вот этого потока:
    
    gulp.src('frontend/styles/**/*styl', {base: 'frontend'})
    		.pipe(debug({title: 'src:'}))
    		.pipe(stylus())
    		.pipe(debug({title: 'stylus:'}))
    
    , чтобы всё объеденить это и передать дальше
    
    stylus: 2 items - завершился поток stylus всего два элемента/
    concat: frontend\all.css - затем сработал concat
    concat: 1 item
    Finished 'styles' after 186 ms

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,15 @@
 const gulp = require('gulp');
 const stylus = require('gulp-stylus');
 const concat = require('gulp-concat');
+const debug = require('gulp-debug');
 
 gulp.task('styles', function() {
 	return gulp.src('frontend/styles/**/*styl', {base: 'frontend'})
+		.pipe(debug({title: 'src:'}))
 		.pipe(stylus())
+		.pipe(debug({title: 'stylus:'}))
 		.pipe(concat('all.css'))
+		.pipe(debug({title: 'concat:'}))
 		.pipe(gulp.dest('public'));
 });
 
