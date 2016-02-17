75e1435e8747cc26130baf42ea7912464d33d024
tsvetkovpro <285019@gmail.com>
2016-02-17 18:02:54 +0300

    Начальная сборка стилей
    stylus - автоматически ищет эти всякие импорты. подключает .
    И если есть много файлов css , я могу файлы импорта (@import './header' ) расположить в нужном порядке

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,8 @@ const concat = require('gulp-concat');
 const debug = require('gulp-debug');
 
 gulp.task('styles', function() {
-	return gulp.src('frontend/styles/**/*styl', {base: 'frontend'})
-		.pipe(debug({title: 'src:'}))
+	return gulp.src('frontend/styles/main.styl', {base: 'frontend'})
 		.pipe(stylus())
-		.pipe(debug({title: 'stylus:'}))
-		.pipe(concat('all.css'))
-		.pipe(debug({title: 'concat:'}))
 		.pipe(gulp.dest('public'));
 });
 
