5ddb5310b8cbf731714d43fd80e34dbeb65d8707
tsvetkovpro <285019@gmail.com>
2016-02-17 17:11:08 +0300

    начальная сборка стилей
    объединение в один

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@
 
 const gulp = require('gulp');
 const stylus = require('gulp-stylus');
+const concat = require('gulp-concat');
 
 gulp.task('styles', function() {
 	return gulp.src('frontend/styles/**/*styl', {base: 'frontend'})
 		.pipe(stylus())
+		.pipe(concat('all.css'))
 		.pipe(gulp.dest('public'));
 });
 
