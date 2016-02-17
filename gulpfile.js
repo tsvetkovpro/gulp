bacb2a64369e81483bb8bfada88f8a8f251ee8fa
tsvetkovpro <285019@gmail.com>
2016-02-17 18:49:23 +0300

    Начальная сборка стилей
    sourcemaps - это специальная структура данных, которая при преобразовании файлов отражает что было и что стало. эта информация потом записывается либо в самом итоговом файле , либо в спец. формате рядом с ним. И браузеры автоматически её читают.
    Получается что когда мы используем инструменты разработки мы можем смотреть, как бы исходные файлы из которых был собран main.css.
    
    .pipe(souecemaps.init()) - один из основных плагинов, написанный разработчиками gulp. и он интегрирован почти со всеми инструментами которые меняют содержимое файлов.
    
    - Когда в него поступает новый файл он создаёт спец. свойство. file.sourceMap. Сначала она пустая, это говорит о том что с файлом ничего не делали. Конечно можно поставить опцию - loadmaps/
    - все плагины знают про это свойство и когда меняют файл отражают изменения там.
    
    в итоге когда мы закончили обработку, мы пайпим
    .pipe(sourcemaps.write()) - добавляет итоговый sourcemaps в сам файл

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@
 
 const gulp = require('gulp');
 const stylus = require('gulp-stylus');
-const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
 const debug = require('gulp-debug');
 
 gulp.task('styles', function() {
-	return gulp.src('frontend/styles/main.styl', {base: 'frontend'})
+	return gulp.src('frontend/styles/main.styl')
+		.pipe(sourcemaps.init())
 		.pipe(stylus())
+		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('public'));
 });
 
