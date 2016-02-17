f0075f7ed2f4a8c93bdf58e4b0c2d65cf6ff7d7b
tsvetkovpro <285019@gmail.com>
2016-02-17 12:43:58 +0300

    Начальная сборка стилей
    файлы в предыдущм коммите попали напрямую в public. что если мы хотим чтобы файлы попадали в директорию
    public\styles
    
    в public попало потому что
    frontend/styles/ - это base . соответственно gulp.dest пишет файлы в public\ и относительный путь, путь относительно dest - **/*styl (header.styl в текущем варианте)
    
    чтобы исправить два варианта:
    
    1. в src указать base в явном виде {base: 'frontend'}

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const gulp = require('gulp');
 const stylus = require('gulp-stylus');
 
 gulp.task('styles', function() {
-	return gulp.src('frontend/styles/**/*styl')
+	return gulp.src('frontend/styles/**/*styl', {base: 'frontend'})
 		.pipe(stylus())
 		.pipe(gulp.dest('public'));
 });
