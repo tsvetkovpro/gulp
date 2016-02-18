7f47a97cf4df3db8bc14caa7abfe41530801ef07
tsvetkovpro <285019@gmail.com>
2016-02-18 19:13:34 +0300

    Начальная сборка стилей
    используем обычный модуль del для удаления. он возвращает promise после завершения
    
    сначала выполняется clean потом две задачи параллельно
    
    gulp.task('build', gulp.series(
    		'clean',
    		gulp.parallel('styles', 'assets')));

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const stylus = require('gulp-stylus');
 const sourcemaps = require('gulp-sourcemaps');
 const debug = require('gulp-debug');
 const gulpIf = require('gulp-if');
+const del = require('del');
 
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
@@ -12,12 +13,21 @@ const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'developm
 gulp.task('styles', function() {
 
 	return gulp.src('frontend/styles/main.styl')
-		.pipe(gulpIf(function(file) {
-				return file.extname == '.js';
-			}, sourcemaps.init()))
+		.pipe(gulpIf(isDevelopment, sourcemaps.init()))
 		.pipe(stylus())
 		.pipe(gulpIf(isDevelopment, sourcemaps.write()))
 		.pipe(gulp.dest('public'));
-
 });
 
+gulp.task('clean', function() {
+	return del('public');
+});
+
+gulp.task('assets', function() {
+	return gulp.src('frontend/assets/**')
+		.pipe(gulp.dest('public'));
+});
+
+gulp.task('build', gulp.series(
+		'clean',
+		gulp.parallel('styles', 'assets')));
